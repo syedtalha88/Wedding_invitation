6b59fc2a3f6df21fe54d1049e8cc1b468891ebdf
syedtalha88 <talhasyed910@gmail.com>
2025-08-10 15:26:31 +0530

    base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/marriage-invitation",
+  base:"/Wedding_invitation",
   server: {
     host: "::",
     port: 8080,
